f061699af7253b29348cd4f7e2fd5b0cb65bc450
Anthony-Kwong <94846235+Anthony-Kwong@users.noreply.github.com>
2023-03-16 13:29:42 +0000

    update

---
+++ make_plots2.R
@@ -0,0 +1,13 @@
+#second attempt at plots for tempo project
+
+#zipf: When birds sing longer songs (by note number), do they sing shorter versions of the note classes?
+
+# one data point per note class
+#y = average note length (across entire pop), x = song duration, col by note class
+
+#menz: are long elements made of smaller bits
+# x = mean note length, y = mean frequency across pop, col by note class
+
+#those ones are statistically significant, America and Asian have much smaller p-valuea. Yes but both are sig. and very small. 
+
+#try the new model and it should be clearer
